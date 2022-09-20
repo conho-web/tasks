76f6e32a52298ac9e47b60a9c7cb5cd30bb6b12d
conho-web <conho@internet.ru>
2022-09-20 15:59:33 +0300

    rate: done task

---
+++ js/rate.js
@@ -0,0 +1,38 @@
+let arrayRate = [];
+
+function getValues() {
+  let from = document.querySelector('.from').value;
+  let to = document.querySelector('.to').value;
+  transfer(from, to);
+}
+
+function transfer(from, to) {
+  from = Date.parse(from);
+  to = Date.parse(to);
+
+  for (let i = from; i < to; i = i + 24 * 60 *60 * 1000){
+    date = new Date(i).toISOString().substr(0, 10);  
+
+    let newDate = date.split('-').map(n => {
+      return parseInt(n)
+    }).join('-')
+
+    getData(newDate);
+  }
+}
+
+function getData(newDate) {
+  fetch(`https://www.nbrb.by/api/exrates/rates/145?ondate=${date}&periodicity=0`)
+    .then((response) => response.json())
+    .then((data) => {
+      arrayRate.push(data.Cur_OfficialRate);
+
+      let minValue = Math.min(...arrayRate);
+      let maxValue = Math.max(...arrayRate);
+
+      document.querySelector('.result').innerHTML = `
+        <p>Минимальный курс: ${minValue}</p>
+        <p>Максимальный курс: ${maxValue}</p>
+      `;
+    });
+}
